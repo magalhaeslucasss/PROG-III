f33358072c453ac24a80c7e5e23b89455b585c7f
TomazC-A-L <165029017+TomazC-A-L@users.noreply.github.com>
2024-04-04 23:25:10 -0300

    Add files via upload
    Protótipo da ferramenta de formatação dos dados. Ainda em desenvolvimento.

--- testeVideo.java
+++ testeVideo.java
@@ -1,30 +1,66 @@
 import java.io.*;
+import java.util.*;
 
 public class testeVideo {
 
+    
     public static void main(String[] args) {
 
+        String[] testeGamer = new String[8];
+        
         try {
-            BufferedReader bf = new BufferedReader(new FileReader("src/main/java/videoTeste.txt"));
+            BufferedReader bf = new BufferedReader(new FileReader("video.txt"));
             String linha = bf.readLine();
-
+            boolean tester = false;
             while (linha != null) {
                 System.out.println(linha);
                 System.out.println("----");
                 String[] campos = linha.split(",");
+                
+                int QuantCampos = campos.length;
+                
+                while (QuantCampos > 8) {
+                    
+                    String[] aux = new String[QuantCampos - 1];
 
-                System.out.println("Code: " + campos[0]);
-                System.out.println("Title: " + campos[1]);
-                System.out.println("Video ID: " + campos[2]);
-                System.out.println("Published At: " + campos[3]);
-                System.out.println("Keyword: " + campos[4]);
-                System.out.println("Likes: " + campos[5]);
-                System.out.println("Comments: " + campos[6]);
-                System.out.println("Views: " + campos[7]);
-                if (campos.length > 8) {
-                    System.out.println("Esse campo não podia existir: " + campos[8]);
+                    for (int i = 0; i < 2; i++){
+                        aux[i] = campos[i];
+                    }
+
+
+                    for (int i = 1; i < aux.length; i++){
+                        if (i+1 == 2) {
+                            aux [i] += campos[i + 1];
+                        } else {
+                            aux[i] = campos [i + 1];
+                        }
+                    }
+                    QuantCampos = aux.length;
+                    testeGamer = aux;
+                    tester = true;
+                    System.out.println(aux.length);
                 }
-
+                if (tester) {
+                    System.out.println("Code: " + testeGamer[0]);
+                    System.out.println("Title: " + testeGamer[1]);
+                    System.out.println("Video ID: " + testeGamer[2]);
+                    System.out.println("Published At: " + testeGamer[3]);
+                    System.out.println("Keyword: " + testeGamer[4]);
+                    System.out.println("Likes: " + testeGamer[5]);
+                    System.out.println("Comments: " + testeGamer[6]);
+                    System.out.println("Views: " + testeGamer[7]);
+                } else {
+                    System.out.println("Code: " + campos[0]);
+                    System.out.println("Title: " + campos[1]);
+                    System.out.println("Video ID: " + campos[2]);
+                    System.out.println("Published At: " + campos[3]);
+                    System.out.println("Keyword: " + campos[4]);
+                    System.out.println("Likes: " + campos[5]);
+                    System.out.println("Comments: " + campos[6]);
+                    System.out.println("Views: " + campos[7]);
+                }
+                    tester = false;
+                
                 // Ler a próxima linha
               System.out.println("-------------");
                 linha = bf.readLine();
